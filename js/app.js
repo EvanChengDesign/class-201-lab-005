02503cf3b0a61c10a6f6b29e631b3a07e03fa5f2
Evan Cheng <evanchengdesign@gmail.com>
2024-02-23 16:36:36 -0500

    add function for multiply

--- js/app.js
+++ js/app.js
@@ -10,8 +10,7 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function sum(a, b) { //eslint-disable-line
   let total = a + b;
-  let result_str = `The sum of ${a} and ${b} is ${total}.`;
-  let result = [total, result_str];
+  let result = [total, `The sum of ${a} and ${b} is ${total}.`];
   return result;
 }
 
@@ -34,11 +33,13 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
-
+  let total = a * b;
+  let result = [total, `The product of ${a} and ${b} is ${total}.`];
+  return result;
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
