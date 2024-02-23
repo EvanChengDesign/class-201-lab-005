de7d6ea2867cd9c4e907e922c9c59e8eaa9c9199
Evan Cheng <evanchengdesign@gmail.com>
2024-02-23 17:09:17 -0500

    function for question-3

--- js/app.js
+++ js/app.js
@@ -35,8 +35,8 @@ Test this function by hand in the console to get it working, and when you think 
 function multiply(a, b) { //eslint-disable-line
   let total = a * b;
   let result = [total, `The product of ${a} and ${b} is ${total}.`];
-  return result; 
-  
+  return result;
+
 }
 
 //let result =multiply(5,9);
@@ -59,11 +59,16 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
+  let totalSum = sum(sum(a,b)[0],c)[0];
+  let totalMultiply = multiply(multiply(a,b)[0],c)[0];
+  let result = [totalSum, totalMultiply, `${a} and ${b} and ${c} sum to ${totalSum}.`, `The product of ${a} and ${b} and ${c} is ${totalMultiply}.`];
+  return result;
+
 
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -108,7 +113,7 @@ function multiplyArray(multArr) { //eslint-disable-line
 // Here is the test for multiplyArray(); uncomment it to run it
 // testMultiplyArray(testArray);
 
-// Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
+// Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop.
 
 // You're done! Submit the link to the repo following the instructions in Canvas. Or, try out the stretch goal below...
 
