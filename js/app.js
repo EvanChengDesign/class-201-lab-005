aa45f609836c33338cfb7461d108b3a05fcdf380
Evan Cheng <evanchengdesign@gmail.com>
2024-02-23 16:31:39 -0500

    fix code for function sum()

--- js/app.js
+++ js/app.js
@@ -9,9 +9,8 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sum(a, b) { //eslint-disable-line
-  let total = a + b;
-  let result_str = `The sum of ${a} and ${b} is ${total}.`;
-  let result = [total, result_str];
+  let total = a + b;  
+  let result = [total, `The sum of ${a} and ${b} is ${total}.`];
   return result;
 }
 
@@ -34,11 +33,15 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
-
+  let total = a * b;
+  let result = `The product of ${a} and ${b} is ${total}.`;
+  return result;
 }
 
+//let result =multiply(5,9);
+
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+//testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
